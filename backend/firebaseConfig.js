6bf837b858cf576797ebac40e8d890594e4e8d34
dor1kk <dorajetkukaj@gmail.com>
2024-07-28 17:28:01 +0200

    updates

--- backend/firebaseConfig.js
+++ backend/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_GWLJhow96YcykZjWzjXP6K53nCc4-48",
-  authDomain: "chatapp-430709.firebaseapp.com",
-  projectId: "chatapp-430709",
-  storageBucket: "chatapp-430709.appspot.com",
-  messagingSenderId: "115594972865",
-  appId: "1:115594972865:web:02a5593dc8e09c01cc8e40",
-  measurementId: "G-P3W0B8Q24T"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
